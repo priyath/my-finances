da2e985741f99843c7b412819a9ca717cc85da02
priyath <priyath.gregory@gmail.com>
2022-11-26 15:29:45 +0800

    initial folder structure, mui integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from "./views/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      My Finances App
+        <Dashboard/>
     </div>
   );
 }
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,9 @@
+import {Box} from "@mui/material";
+
+const Dashboard = () => {
+    return <Box>
+        Dashboard for My Finances
+    </Box>
+};
+
+export default Dashboard;
