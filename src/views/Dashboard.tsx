16fc4bbf67fde875e5933c516406f381c945c0dc
priyath <priyath.gregory@gmail.com>
2022-11-26 16:00:01 +0800

    theme integration and dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,9 +1,20 @@
 import {Box} from "@mui/material";
+import PermanentDrawerLeft from "../components/PermanentDrawerLeft";
+import Toolbar from "@mui/material/Toolbar";
+import * as React from "react";
 
 const Dashboard = () => {
-    return <Box>
-        Dashboard for My Finances
-    </Box>
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <PermanentDrawerLeft/>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+                <Toolbar />
+            </Box>
+        </Box>
+    )
 };
 
 export default Dashboard;
